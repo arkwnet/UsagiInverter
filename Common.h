362ec9083bfbc7c9692f93c3020b8ed6a8d56202
Sora Arakawa <mail@arkw.net>
2023-08-15 16:41:47 +0900

    First commit

---
+++ Common.h
@@ -0,0 +1,23 @@
+﻿#ifndef _common_h
+#define _common_h
+
+static const wchar_t VERSION[14] = L"Version 1.0.0";
+static const int WINDOW_WIDTH = 720;
+static const int WINDOW_HEIGHT = 480;
+static const int COLOR_WHITE = GetColor(245, 245, 245);
+
+class Fps {
+	int mStartTime;
+	int mCount;
+	float mFps;
+	static const int N = 30;
+	static const int FPS = 30;
+	public:
+		Fps();
+		bool Update();
+		void Draw();
+		void Wait();
+		int Get();
+};
+
+#endif
---
+++ Main.cpp
@@ -0,0 +1,31 @@
+﻿/*
+うさぎインバータ
+(c) 2023 Sora Arakawa all rights reserved.
+*/
+
+#include "DxLib.h"
+#include "Common.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	SetUseCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
+	ChangeWindowMode(TRUE);
+	SetFullScreenResolutionMode(DX_FSRESOLUTIONMODE_NATIVE);
+	SetUseDirect3DVersion(DX_DIRECT3D_9);
+	SetWindowIconID(813);
+	SetMainWindowText(L"うさぎインバータ");
+	SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32);
+	if (DxLib_Init() == -1) {
+		return -1;
+	}
+	Fps fps;
+
+	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+		fps.Update();
+		DrawBox(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, COLOR_WHITE, TRUE);
+		fps.Wait();
+	}
+
+	DxLib_End();
+	return 0;
+}
